03cab9a4ef8faa204344b3c05eb155b1d9412328
VinDiesel1989 <410885117@qq.com>
2024-12-26 19:18:42 +0800

    面试题：合并数组

--- src/main/java/Test2.java
+++ src/main/java/Test2.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Test2 {
 
-    //    给出两个已经按照非递减顺序排列好的整数数组nums1和nums2，同时给定两个整数m和n，
+//    给出两个已经按照非递减顺序排列好的整数数组nums1和nums2，同时给定两个整数m和n，
 //    分别代表nums1和nums2中的元素数量。
 //    需要将nums2中的元素合并到nums1中，使得合并后的数组仍然保持非递减顺序排列。
 //    注意：最终的合并结果不需要通过函数返回，而是需要保存在数组nums1中。
@@ -17,13 +17,27 @@ public class Test2 {
         while (i >= 0 && j >= 0) {
 
             if (nums1[i] > nums2[j]) {
-
                 nums1[k--] = nums1[i--];
             } else {
                 nums1[k--] = nums2[j--];
             }
-
             while (j >= 0) {
+                nums1[k--] = nums2[j--];
+            }
+        }
+    }
+
+    public static void merge2(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while (i >= 0 && j >= 0) {
+
+            if (nums1[i] > nums2[j]) {
+
+                nums1[k--] = nums1[i--];
+            } else {
                 nums1[k--] = nums2[j--];
             }
         }
